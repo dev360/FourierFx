e60538083592b62f9c4bb572ebec96e9d8ab668d
Christian Toivola <c.toivola@dev360.com>
2012-01-15 02:34:06 -0500

    Adding latest version

--- src/quotes/QuotePublisher.hs
+++ src/quotes/QuotePublisher.hs
@@ -1,6 +1,7 @@
 import Control.Applicative
 import Control.Monad
 import System.IO
+import System.IO.Error
 import System.Exit
 import System.Environment
 import qualified System.ZMQ3 as ZMQ
@@ -8,19 +9,26 @@ import qualified Data.ByteString.UTF8 as SB
 import qualified Data.ByteString.Char8 as SB
 import Codec.Binary.UTF8.String(encode)
 
+
 -- | main program loop
 main :: IO ()
 main = do
     args <- getArgs
     when (length args /= 1) $ do
-        hPutStrLn stderr "usage: prompt <address> <username>"
+        hPutStrLn stderr "usage: prompt <address>"
         exitFailure
     let addr = args !! 0
         name = SB.append (SB.fromString "test") (SB.fromString ": ")
+    
     ZMQ.withContext 1 $ \c ->
         ZMQ.withSocket c ZMQ.Pub $ \s -> do
             ZMQ.bind s addr
             forever $ do
-                line <- SB.fromString <$> getLine
-                ZMQ.send s [] (SB.append name line)
+                line <- SB.fromString <$> getLine `catch`
+                    \e -> if isEOFError e then return "" 
+                        else ioError e
 
+                if line /= (SB.fromString "") then
+                    ZMQ.send s [] (SB.append name line)
+                else
+                    return ()                    
--- src/quotes/QuoteSubscriber.hs
+++ src/quotes/QuoteSubscriber.hs
@@ -1,23 +1,25 @@
+import Control.Applicative
 import Control.Monad
 import System.IO
 import System.Exit
 import System.Environment
-import Control.Exception
 import qualified System.ZMQ3 as ZMQ
-import qualified Data.ByteString as SB
+import qualified Data.ByteString.UTF8 as SB
+import qualified Data.ByteString.Char8 as SB
+import Codec.Binary.UTF8.String(encode)
 
+-- | main program loop
 main :: IO ()
 main = do
     args <- getArgs
-    when (length args < 1) $ do
-        hPutStrLn stderr "usage: display <address> [<address>, ...]"
+    when (length args /= 1) $ do
+        hPutStrLn stderr "usage: prompt <address> <username>"
         exitFailure
+    let addr = args !! 0
+        name = SB.append (SB.fromString "test") (SB.fromString ": ")
     ZMQ.withContext 1 $ \c ->
-        ZMQ.withSocket c ZMQ.Sub $ \s -> do
-            ZMQ.subscribe s ""
-            mapM (ZMQ.connect s) args
+        ZMQ.withSocket c ZMQ.Pub $ \s -> do
+            ZMQ.bind s addr
             forever $ do
-                line <- ZMQ.receive s
-                SB.putStrLn line
-                hFlush stdout
-                
+                line <- SB.fromString <$> getLine
+                ZMQ.send s [] (SB.append name line)
