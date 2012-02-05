8e213ed5f4365fda828d29ddc5351c7c51c19c7f
Christian Toivola <c.toivola@dev360.com>
2012-02-05 02:09:31 -0500

    Removed old file structure

--- src/hs/quotes/cabal-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-for line in `cat cabal.requirements.txt`
-  do
-    cabal install $line;
-done
--- src/hs/quotes/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf bin/*
-ghc -o bin/quote_publisher QuotePublisher.hs
-ghc -o bin/quote_subscriber QuoteSubscriber.hs
